6d6da80c888ceb04e9d1726a03339098aabe7ead
YiTsen <tsen.991007@gmail.com>
2026-07-26 18:30:32 +0800

    [Feeder] other: 把原本分支的 IntakeSubsystem 和 RobotContainer 的內容刪掉

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,20 +4,11 @@
 
 package frc.robot;
 
-import java.io.ObjectInputFilter.Config;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.SwerveControlCmd;
-import frc.robot.subsystems.swervedrive.SwerveDrive;
-import frc.robot.subsystems.swervedrive.WpilibSwerveDrive;
 
 public class RobotContainer {
-  private final SwerveDrive swerveDrive;
-  private final CommandXboxController mainController = new CommandXboxController(0);
   public RobotContainer() {
-    swerveDrive.setDefaultCommand(new SwerveControlCmd(swerveDrive, mainController, null, null););
     configureBindings();
   }
 
