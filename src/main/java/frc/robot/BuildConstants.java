cb12506382078069b6eae284a0ca5e143c0f5720
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-07-26 14:28:09 +0800

    [BuildConstants] feat: add gversion plugin and generate BuildConstants for versioning

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026Overlooking-offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "00945ee31b77ea98deb5e712b606a6e931805f9f";
+  public static final String GIT_DATE = "2026-04-24 20:19:37 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-07-26 14:20:20 CST";
+  public static final long BUILD_UNIX_TIME = 1785046820616L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
