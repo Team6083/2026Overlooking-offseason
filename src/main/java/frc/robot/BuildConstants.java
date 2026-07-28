50ad0bf439df590d88ccd0ad1c205c6ac81c2cba
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-07-28 21:31:29 +0800

    [BuildConstants] feat: update Robot class to publish build and git information to NetworkTable and SmartDashboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Overlooking-offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "a12b796fc5187723228538c83bfd888bc8be9f0c";
-  public static final String GIT_DATE = "2026-07-28 10:11:52 CST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "d582e319d67e1ab83ee57faf1bdfc485358b8e06";
+  public static final String GIT_DATE = "2026-07-28 21:24:31 CST";
   public static final String GIT_BRANCH = "Monday/BuildConstants";
-  public static final String BUILD_DATE = "2026-07-28 21:20:15 CST";
-  public static final long BUILD_UNIX_TIME = 1785244815753L;
+  public static final String BUILD_DATE = "2026-07-28 21:30:56 CST";
+  public static final long BUILD_UNIX_TIME = 1785245456574L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
