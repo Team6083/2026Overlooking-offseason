d582e319d67e1ab83ee57faf1bdfc485358b8e06
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-07-28 21:24:31 +0800

    [BuildConstants] feat: update BuildConstants with latest versioning information and add to .gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Overlooking-offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "cb12506382078069b6eae284a0ca5e143c0f5720";
-  public static final String GIT_DATE = "2026-07-26 14:28:09 CST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "a12b796fc5187723228538c83bfd888bc8be9f0c";
+  public static final String GIT_DATE = "2026-07-28 10:11:52 CST";
   public static final String GIT_BRANCH = "Monday/BuildConstants";
-  public static final String BUILD_DATE = "2026-07-28 10:10:41 CST";
-  public static final long BUILD_UNIX_TIME = 1785204641130L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-07-28 21:20:15 CST";
+  public static final long BUILD_UNIX_TIME = 1785244815753L;
+  public static final int DIRTY = 1;
+
   private BuildConstants(){}
 }
