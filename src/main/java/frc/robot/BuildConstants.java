a12b796fc5187723228538c83bfd888bc8be9f0c
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-07-28 10:11:52 +0800

    [Shooter] feat: update BuildConstants with latest versioning information

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Overlooking-offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "00945ee31b77ea98deb5e712b606a6e931805f9f";
-  public static final String GIT_DATE = "2026-04-24 20:19:37 CST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-26 14:20:20 CST";
-  public static final long BUILD_UNIX_TIME = 1785046820616L;
-  public static final int DIRTY = 1;
-
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "cb12506382078069b6eae284a0ca5e143c0f5720";
+  public static final String GIT_DATE = "2026-07-26 14:28:09 CST";
+  public static final String GIT_BRANCH = "Monday/BuildConstants";
+  public static final String BUILD_DATE = "2026-07-28 10:10:41 CST";
+  public static final long BUILD_UNIX_TIME = 1785204641130L;
+  public static final int DIRTY = 0;
   private BuildConstants(){}
 }
