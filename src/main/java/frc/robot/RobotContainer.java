bf41a17d6725eb3285044c37cc111a7e645af729
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-07-30 02:21:08 +0800

    [Swerve] feat: 新增除了按鍵以外的底盤控制

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.swervedrive.SwerveDrive;
+import frc.robot.subsystems.swervedrive.SwerveDriveFactory;
 
 public class RobotContainer {
+  private final SwerveDrive swerveDrive;
   public RobotContainer() {
+    swerveDrive = SwerveDriveFactory.createSwerveDrive(
+        SwerveDriveFactory.SwerveImplementation.WPILIB,
+        SwerveDriveFactory.RobotVariant.TEST);
     configureBindings();
   }
 
