cfb1d72c87ed49dd6e56d65751b596653c7d64d2
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-07-30 12:04:13 +0800

    [Swerve] feat: 把操控底盤需要的 RobotContainer 寫完

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,22 @@
 
 package frc.robot;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.SwerveControlCmd;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
 import frc.robot.subsystems.swervedrive.SwerveDriveFactory;
 
 public class RobotContainer {
   private final SwerveDrive swerveDrive;
+  private final CommandXboxController mainController = new CommandXboxController(0);
+  private Supplier<Boolean> shouldSprint = () -> mainController.leftBumper().getAsBoolean();
+  private Supplier<Boolean> shouldLockPose = () -> mainController.a().getAsBoolean();
   public RobotContainer() {
     swerveDrive = SwerveDriveFactory.createSwerveDrive(
         SwerveDriveFactory.SwerveImplementation.WPILIB,
@@ -18,7 +27,14 @@ public class RobotContainer {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    swerveDrive.setDefaultCommand(new SwerveControlCmd(
+        swerveDrive, mainController, shouldSprint, shouldLockPose));
+    mainController.start().onTrue(Commands.runOnce(() -> {
+      swerveDrive.zeroGyro();
+      swerveDrive.resetPose(new Pose2d(swerveDrive.getPose2d().getTranslation(), Rotation2d.fromDegrees(0)));
+    }));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
