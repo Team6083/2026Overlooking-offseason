8f04e79cc627ceb90c92e4ac1749b4fbbf40e14e
YiTsen <tsen.991007@gmail.com>
2026-08-02 10:28:24 +0800

    [Intake] feat: Intake interface

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+public interface Intake {
+
+  void intake();
+
+  void reverseIntake();
+
+  void stopIntake();
+
+  void deploy();
+
+  void retract();
+
+  void manualDeploy();
+
+  void manualRetract();
+}
