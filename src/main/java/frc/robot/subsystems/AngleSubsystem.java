0ae3d5b249d47d8ef80e0ca4edcb299f7d9f2a4d
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-08-09 19:03:14 +0800

    [Shooter} checkStyle:

--- src/main/java/frc/robot/subsystems/AngleSubsystem.java
+++ src/main/java/frc/robot/subsystems/AngleSubsystem.java
@@ -5,16 +5,14 @@
 package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
-
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
