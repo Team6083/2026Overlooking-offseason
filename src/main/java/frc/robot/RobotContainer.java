39cf322878fb8db958e49e5f12b1330df0f08898
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-08-09 19:26:47 +0800

    [Shooter} checkStyle:

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.AngleSubsystem;
 import frc.robot.subsystems.AngleSubsystem.AnglePreset;
 import frc.robot.subsystems.FeederSubsystem;
@@ -46,8 +46,7 @@ public class RobotContainer {
     mainController.b().onTrue(angleSubsystem.adjustAngleCmd(AnglePreset.TRANS));
     mainController.x().onTrue(angleSubsystem.adjustAngleCmd(AnglePreset.SHOOT));
     mainController.y().onTrue(Commands.runOnce(angleSubsystem::lockCurrentAngle, angleSubsystem));
-}
-  
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
