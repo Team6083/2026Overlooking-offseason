eb68c4db2503be214def90df94976fe61ba7d750
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-08-13 01:34:44 +0800

    [Shooter] feat: 新增固定位置和仰角的 command

---
+++ src/main/java/frc/robot/commands/manualShooterComboCmd.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.TransportSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class manualShooterComboCmd extends ParallelCommandGroup {
+  public manualShooterComboCmd(
+    ShooterSubsystem shooterSubsystem,
+    FeederSubsystem feederSubsystem,
+    TransportSubsystem transportSubsystem) {
+    addCommands(
+      shooterSubsystem.shootCmd(),
+      Commands.idle().until(shooterSubsystem::isShooterAtSpeed).andThen(
+        Commands.parallel(
+          feederSubsystem.feedInCmd(),
+          transportSubsystem.transportInCmd()
+        )
+      )
+    );
+  }
+}
