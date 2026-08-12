189ab9a6af923174d484a5b2e7cf166fe871a24b
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-08-13 01:59:18 +0800

    [Intake] value: 改 intake 轉速

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,8 +127,8 @@ public static class AutoConstants {
       public static final boolean intakeInverted = false;
       public static final boolean pivotInverted = true;
 
-      public static final double intakeSpeed = 0.75;
-      public static final double reverseIntakeSpeed = -0.75;
+      public static final double intakeSpeed = 0.65;
+      public static final double reverseIntakeSpeed = -0.65;
 
       public static final double pivotManualSpeed = 0.3;
       public static final double pivotManualRetractSpeed = 0.3;
