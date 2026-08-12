2468ad4b8bf3c9503f38ea1675f03da2ab8a2f56
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-08-12 09:21:09 +0800

    [Competition] feat: 新增儲存 log 檔的程式碼

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+import com.revrobotics.util.StatusLogger;
+
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,9 +21,23 @@ public class Robot extends TimedRobot {
 
   private final NetworkTableInstance ntInstance = NetworkTableInstance.getDefault();
   private final RobotContainer m_robotContainer;
+  private boolean savelog = false;
+  private Timer gcTimer = new Timer();
 
   public Robot() {
+    if (!savelog) {
+      StatusLogger.disableAutoLogging();
+    }
+
     m_robotContainer = new RobotContainer();
+    gcTimer.start();
+
+    if (savelog) {
+      DataLogManager.start();
+      DriverStation.startDataLog(DataLogManager.getLog());
+    }
+
+    SignalLogger.enableAutoLogging(savelog);
 
     ntInstance.getStringTopic("/Metadata/BuildDate").publish()
         .set(BuildConstants.BUILD_DATE);
