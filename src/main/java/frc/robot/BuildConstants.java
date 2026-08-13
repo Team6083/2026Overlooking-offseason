e474ee73f55a145a3a38a6f4dfc90ca925ccdf8f
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-08-13 13:41:08 +0800

    傳程式 BuildConstants 數據改變

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026Overlooking-offseason-1";
+  public static final String MAVEN_NAME = "2026Overlooking-offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 236;
-  public static final String GIT_SHA = "7b5117a81dd4c3390ea87c4b25e6c05d71c3cdef";
-  public static final String GIT_DATE = "2026-08-13 12:37:07 TST";
+  public static final int GIT_REVISION = 241;
+  public static final String GIT_SHA = "966f300d750529e1abdc48b2a2369d03f66dbc56";
+  public static final String GIT_DATE = "2026-08-13 13:23:53 TST";
   public static final String GIT_BRANCH = "Competition/0813";
-  public static final String BUILD_DATE = "2026-08-13 13:14:34 TST";
-  public static final long BUILD_UNIX_TIME = 1786598074853L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-08-13 13:32:10 TST";
+  public static final long BUILD_UNIX_TIME = 1786599130481L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
