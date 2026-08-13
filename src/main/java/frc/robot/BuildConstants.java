6ff3deef5b2a8d47a7f587b50df6537656f75b3f
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-08-13 10:57:09 +0800

    [Auto] feat: update build constants for versioning and git information

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Overlooking-offseason-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 170;
-  public static final String GIT_SHA = "7cf65b326ebd574cbbb56b06c9778f7987a7b9af";
-  public static final String GIT_DATE = "2026-08-11 21:34:22 CST";
-  public static final String GIT_BRANCH = "Monday/Shooter/AdjustSpeedAngle";
-  public static final String BUILD_DATE = "2026-08-11 22:05:19 CST";
-  public static final long BUILD_UNIX_TIME = 1786457119397L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 200;
+  public static final String GIT_SHA = "6de0d67dec69e151d3a3d40a152e881f1db35e1a";
+  public static final String GIT_DATE = "2026-08-12 22:56:01 TST";
+  public static final String GIT_BRANCH = "Auto/PathPlanner";
+  public static final String BUILD_DATE = "2026-08-13 10:53:31 TST";
+  public static final long BUILD_UNIX_TIME = 1786589611551L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
