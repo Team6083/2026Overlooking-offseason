cbcb6db07376f5c988ce3f4e3fd68d94787e9d19
Maureen1031 <j120380@my.cmsh.cyc.edu.tw>
2026-08-20 00:33:36 +0800

    feat: BuildConstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
-public class BuildConstants {
-    
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026Overlooking-offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 59;
+  public static final String GIT_SHA = "c0670ab76a02e263437e4bd953dfe7101926303d";
+  public static final String GIT_DATE = "2026-08-09 16:09:25 TST";
+  public static final String GIT_BRANCH = "Monday/Intake/IntakeBasicFunction";
+  public static final String BUILD_DATE = "2026-08-09 16:35:33 TST";
+  public static final long BUILD_UNIX_TIME = 1786264533781L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
 }
