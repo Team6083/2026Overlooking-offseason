4586979081e26c9a14f19604c371a645d7c0eb37
Elena <j110335@my.cmsh.cyc.edu.tw>
2026-08-26 11:23:17 +0800

    fix: update rotation values in path and restore BuildConstants settings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026Overlooking-offseason";
+  public static final String MAVEN_NAME = "2026Overlooking-offseason-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "c0670ab76a02e263437e4bd953dfe7101926303d";
-  public static final String GIT_DATE = "2026-08-09 16:09:25 TST";
-  public static final String GIT_BRANCH = "Monday/Intake/IntakeBasicFunction";
-  public static final String BUILD_DATE = "2026-08-09 16:35:33 TST";
-  public static final long BUILD_UNIX_TIME = 1786264533781L;
+  public static final int GIT_REVISION = 274;
+  public static final String GIT_SHA = "741d295aa9864ffba19328bcdb1ad40d140a0d5f";
+  public static final String GIT_DATE = "2026-08-20 17:11:21 TST";
+  public static final String GIT_BRANCH = "Competition/0814";
+  public static final String BUILD_DATE = "2026-08-26 11:21:33 TST";
+  public static final long BUILD_UNIX_TIME = 1787714493071L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
